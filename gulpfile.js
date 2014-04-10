e956fa6125c7e646e73fb38c1c8428f70399caa9
Pavel Lasarev <paullasarev@gmail.com>
2014-04-10 10:43:04 +0400

    add gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('clean', function() {
+    gulp.src('public/**/*', {read: false})
+        .pipe(clean());
 });
+
+gulp.task('html', function() {
+ return gulp.src('app/**/*.html')
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('components', function() {
+ return gulp.src('bower_components/**/*')
+    .pipe(gulp.dest('public/bower_components'));
+});
+
+gulp.task('css', function() {
+ return gulp.src('app/styles/**/*')
+    .pipe(gulp.dest('public/styles'));
+});
+
+gulp.task('default', ['html', 'components', 'css']);
